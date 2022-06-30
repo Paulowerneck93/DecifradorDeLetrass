c5465574782866b41c0030760125a0cef8cdd358
Paulo Henrique <80072894+Paulowerneck93@users.noreply.github.com>
2022-06-30 18:25:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    
+    ArrayList<Palavra> listaDePalavras = new ArrayList<Palavra>();
+  
+    listaDePalavras.add(new Palavra ("O lanche está dois reais!"));
+    int var = listaDePalavras.get(0).getPalavra().split(" ", -1).length - 1;
+    String var2[] = new String[var];
+    var2 = listaDePalavras.get(0).getPalavra().split(" ");
+
+
+    System.out.println(listaDePalavras.get(0).getPalavra());
+    for(int i = var; 0 <= i; i--){
+      System.out.println(var2[i].length() + " - " + var2[i]);
+    }
+  
+  }
+}
---
+++ Palavra.java
@@ -0,0 +1,17 @@
+class Palavra{
+  String palavra;
+  int numeroDeLetra;
+
+  Palavra (String frase){
+    this.palavra = frase;
+  }
+
+  String getPalavra(){
+    return palavra;
+  }
+
+  int getNumeroLetra(){
+    return numeroDeLetra;
+  }
+  
+}
